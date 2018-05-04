b107ea7697f5ef6a217505e6a5981f6bbcf499dd
Saran Krishnasamy <ksk.3393@gmail.com>
2018-05-04 11:25:57 -0400

    Some UI tweaks

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,20 @@
 
 $(function(){
     $('.loader').hide();
-    $('#confirm-btn').hide();
+    $("#wait-text").hide();
+    $('#confirm-button').hide();
     $('#yes-button').on('click', function () {
         $('#yes-button').hide();
+        $('#confirm-button').hide();
         $('.loader').show();
         $(".loader").delay(5000).hide(0);
-        //$('#confirm-btn').show();
+        $('#wait-text').show();
+        $("#wait-text").delay(5000).hide(0);
         console.log('Waiting for Authorization...');
+        $("#intro-text").hide();
+        $('#confirm-button').delay(5000).show(0);
     });
+    //$('#confirm-button').show();
 
 });
 
